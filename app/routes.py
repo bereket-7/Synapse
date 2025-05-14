964d5cdd1d7bc42a524d3f6a4913d6f37767c744
bereket-7 <bekhonelign@gmail.com>
2025-05-14 04:23:33 +0300

    Add task management routes for viewing and creating tasks

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required
-from app.models import User
-from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import Task, User
+from app.forms import LoginForm, RegistrationForm, TaskForm
 
 main = Blueprint('main', __name__)
 
@@ -41,3 +41,29 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+
+@main.route('/tasks')
+@login_required
+def tasks():
+    user_tasks = Task.query.filter_by(user_id=current_user.id).all()
+    return render_template('tasks.html', tasks=user_tasks)
+
+
+@main.route('/tasks/create', methods=['GET', 'POST'])
+@login_required
+def create_task():
+    form = TaskForm()
+    if form.validate_on_submit():
+        task = Task(
+            title=form.title.data,
+            description=form.description.data,
+            due_date=form.due_date.data,
+            priority=form.priority.data,
+            author=current_user
+        )
+        db.session.add(task)
+        db.session.commit()
+        flash('Task created!', 'success')
+        return redirect(url_for('main.tasks'))
+    return render_template('create_task.html', form=form)
