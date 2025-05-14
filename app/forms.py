10d4ee6d9bdccfce5b7cfa79ef0ff0333c11f46c
bereket-7 <bekhonelign@gmail.com>
2025-05-14 04:23:25 +0300

    Add TaskForm for task creation with title, description, due date, and priority fields

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import TextAreaField, SelectField, DateTimeField
 
 
 class LoginForm(FlaskForm):
@@ -18,3 +19,12 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[
                                      DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+
+class TaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    due_date = DateTimeField('Due Date', format='%Y-%m-%d %H:%M')
+    priority = SelectField('Priority', choices=[(
+        'Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')])
+    submit = SubmitField('Save Task')
